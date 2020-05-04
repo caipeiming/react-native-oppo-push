d16a222b20751c0e6530fd45228786a747bdd732
caipeiming <394912998@qq.com>
2020-05-04 10:58:45 +0800

    删除 示例 example 的 ios 模块

--- android/src/main/java/top/cpming/rn/push/oppo/RNOppoPushModule.java
+++ android/src/main/java/top/cpming/rn/push/oppo/RNOppoPushModule.java
@@ -3,20 +3,18 @@ package top.cpming.rn.push.oppo;
 
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
-import com.facebook.react.bridge.ReactMethod;
-import com.facebook.react.bridge.Callback;
 
 public class RNOppoPushModule extends ReactContextBaseJavaModule {
 
-  private final ReactApplicationContext reactContext;
+    private final ReactApplicationContext reactContext;
 
-  public RNOppoPushModule(ReactApplicationContext reactContext) {
-    super(reactContext);
-    this.reactContext = reactContext;
-  }
+    public RNOppoPushModule(ReactApplicationContext reactContext) {
+        super(reactContext);
+        this.reactContext = reactContext;
+    }
 
-  @Override
-  public String getName() {
-    return "RNOppoPush";
-  }
+    @Override
+    public String getName() {
+        return "RNOppoPush";
+    }
 }
--- android/src/main/java/top/cpming/rn/push/oppo/RNOppoPushPackage.java
+++ android/src/main/java/top/cpming/rn/push/oppo/RNOppoPushPackage.java
@@ -1,28 +1,23 @@
 
 package top.cpming.rn.push.oppo;
 
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
-
 import com.facebook.react.ReactPackage;
 import com.facebook.react.bridge.NativeModule;
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.uimanager.ViewManager;
-import com.facebook.react.bridge.JavaScriptModule;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
 public class RNOppoPushPackage implements ReactPackage {
     @Override
     public List<NativeModule> createNativeModules(ReactApplicationContext reactContext) {
-      return Arrays.<NativeModule>asList(new RNOppoPushModule(reactContext));
-    }
-
-    // Deprecated from RN 0.47
-    public List<Class<? extends JavaScriptModule>> createJSModules() {
-      return Collections.emptyList();
+        return Arrays.<NativeModule>asList(new RNOppoPushModule(reactContext));
     }
 
     @Override
     public List<ViewManager> createViewManagers(ReactApplicationContext reactContext) {
-      return Collections.emptyList();
+        return Collections.emptyList();
     }
 }
