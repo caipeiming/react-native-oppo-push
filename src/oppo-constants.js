2571cee3f134a4900034b1c827ae2038526c141b
caipeiming <394912998@qq.com>
2020-06-06 12:12:05 +0800

    修复在ios下运行，出现空值的错误

--- src/oppo-constants.js
+++ src/oppo-constants.js
@@ -1,8 +1,9 @@
+import { Platform } from 'react-native';
 import { OppoPushModule } from './native-module'
 
-export const OT_REGISTER = OppoPushModule.OT_REGISTER;
-export const OT_UN_REGISTER = OppoPushModule.OT_UN_REGISTER;
-export const OT_GET_PUSH_STATUS = OppoPushModule.OT_GET_PUSH_STATUS;
-export const OT_GET_NOTIFICATION_STATUS = OppoPushModule.OT_GET_NOTIFICATION_STATUS;
-export const OT_SET_PUSH_TIME = OppoPushModule.OT_SET_PUSH_TIME;
-export const OT_ERROR = OppoPushModule.OT_ERROR;
+export const OT_REGISTER = Platform.OS === "ios" ? null : OppoPushModule.OT_REGISTER;
+export const OT_UN_REGISTER = Platform.OS === "ios" ? null : OppoPushModule.OT_UN_REGISTER;
+export const OT_GET_PUSH_STATUS = Platform.OS === "ios" ? null : OppoPushModule.OT_GET_PUSH_STATUS;
+export const OT_GET_NOTIFICATION_STATUS = Platform.OS === "ios" ? null : OppoPushModule.OT_GET_NOTIFICATION_STATUS;
+export const OT_SET_PUSH_TIME = Platform.OS === "ios" ? null : OppoPushModule.OT_SET_PUSH_TIME;
+export const OT_ERROR = Platform.OS === "ios" ? null : OppoPushModule.OT_ERROR;
